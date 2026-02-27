14347d18518ede74acc433cc3fde3fea454bd878
Yasin <mdyasin399.my@gmail.com>
2026-02-27 15:38:08 +0600

    Add multiply function and call from main

--- src/main.py
+++ src/main.py
@@ -5,10 +5,11 @@ print(name)
 print(today)
 
 
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Addition: ", add(10, 10))
 print("Subtraction: ", subtract(20, 10))
+print("Multiplication: ", multiply(10, 10))
 
 
 
