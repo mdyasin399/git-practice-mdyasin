87dce6a1af913222c8e3abf975b2e68bc384b451
Yasin <mdyasin399.my@gmail.com>
2026-02-27 15:42:51 +0600

    Add divide function and call from main

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from utils import add, subtract, multiply
 print("Addition: ", add(10, 10))
 print("Subtraction: ", subtract(20, 10))
 print("Multiplication: ", multiply(10, 10))
+print("Division:", divide(10, 2))
 
 
 
